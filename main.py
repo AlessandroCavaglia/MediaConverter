333cc7a93a3e4aee276da81511b0eead3b0335db
Alessandro <cavagliascuola@gmail.com>
2022-05-03 20:13:11 +0200

    Completed the convertion to H264 and added the loop with time delay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import subprocess
+import time
+
+THREADS = 6
+DELAY = 15
 
 
 def getListOfFiles(dirName):    #Function found on the web
@@ -33,6 +37,17 @@ def getResolution(file):
     y=int(resolution.split("x")[1])
     return (x,y)
 
+def getCodec(file):
+    codec = str(subprocess.Popen(
+        " ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 \'" + file+"\'", shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0])
+    if codec=="":
+        return ""
+    codec = codec.replace("b", "")
+    codec = codec.replace("\\n", "")
+    codec = codec.replace("'", "")
+    return codec
+
 #Function that converts the file to 1920x1080 then removes the old file
 def convertTo1080(file):
     splittedFile=file.split("/")
@@ -42,7 +57,7 @@ def convertTo1080(file):
         outputFile=outputFile+part+"/"
     outputFile = outputFile[:-1]
 
-    child = subprocess.Popen("ffmpeg -i '"+file+"' -vf scale=1920:1080 '"+outputFile+"' -threads 10",
+    child = subprocess.Popen("ffmpeg -i '"+file+"' -vf scale=1920:1080 '"+outputFile+"' -threads "+THREADS,
         shell=True,
        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     streamdata = child.communicate()[0]
@@ -76,15 +91,64 @@ def convertTo1080(file):
             shell=True,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0])
 
+def convertToH264(file):
+    splittedFile=file.split("/")
+    splittedFile[-1]="NEW_"+splittedFile[-1]
+    outputFile=""
+    for part in splittedFile:
+        outputFile=outputFile+part+"/"
+    outputFile = outputFile[:-1]
+    child = subprocess.Popen("ffmpeg -i '"+file+"' -map 0 -c:v libx264 -crf 18 -vf format=yuv420p -c:a copy '"+outputFile+"'",
+        shell=True,
+       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    streamdata = child.communicate()[0]
+    print(streamdata)
+    rc = child.returncode
+    print(rc)
+    if rc==0 or rc=='0' or rc=="0":
+        codec = getCodec(outputFile)
+        print("New codec: " + codec)
+        if codec=="h264" or codec=='h264':
+            print("Removing file "+file)
+            removeCommand = str(subprocess.Popen(
+               "rm \'"+file+"\'",
+                shell=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0])
+            print("Renaming file "+outputFile+" to "+file)
+            moveCommand = str(subprocess.Popen(
+            "mv \'" + outputFile + "\' \'"+file+"\'",
+            shell=True,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0])
+        else:
+            print("Wrong codec of the new Output found, cleaning")
+            print("Removing file " + file)
+            removeCommand = str(subprocess.Popen(
+                "rm \'" + outputFile + "\'",
+                shell=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0])
+    else:
+        print("Error during conversion, cleaning")
+        print("Removing file " + file)
+        removeCommand = str(subprocess.Popen(
+            "rm \'" + outputFile + "\'",
+            shell=True,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0])
+
 if __name__ == '__main__':
-    files = getListOfFiles("series")
-    for file in files:
-        print(file)
-        (resolutionX,resolutionY) = getResolution(file)
-        print(str(resolutionX)+" "+str(resolutionY))
-        if resolutionX>1920 and resolutionY>1080 and resolutionX%1920==0 and resolutionY%1080==0:
-            print("Converting to 1920x1080:")
-            convertTo1080(file)
-            print("Conversion ended")
-
-
+    while True:
+        files = getListOfFiles("series")
+        for file in files:
+            print(file)
+            (resolutionX,resolutionY) = getResolution(file)
+            print(str(resolutionX)+" "+str(resolutionY))
+            if resolutionX>1920 and resolutionY>1080 and resolutionX%1920==0 and resolutionY%1080==0:
+                print("Converting to 1920x1080:")
+                convertTo1080(file)
+                print("Conversion ended")
+            codec=getCodec(file)
+            print("Coded: "+codec)
+            if codec=="hevc":
+                print("Converting to h264")
+                convertToH264(file)
+                print("Conversion ended")
+            time.sleetp(60*DELAY)
--- readme.md
+++ readme.md
@@ -1,17 +1,21 @@
 # Python media converter
 
 This project aims to solve a specific problem, i have legaly obtained files on my web server that i like to access
-whit JellyFin, some files are 4K resolution and have some HVEC encoding that my ipad AIR can't seem to work with.
+whit JellyFin, some files are 4K resolution and have some HEVC encoding that my ipad AIR can't seem to work with.
 ## Objectives
 
 Create a script that looks for media files, check if resolution is greater than 1920x1080 and converts it in that resolution,
-then checks for the format and if it is HVEC converts it   in a format that my i pad air can work with (H264). 
+then checks for the format and if it is HEVC converts it   in a format that my i pad air can work with (H264). 
 All the conversion is done with ffmpeg via command line so you don't have to install python libraries.
 The project is ment to be run only on linux because it uses heavily the command line to simplify my work.
 ## Project Overview
-ATM you can run the script and specify in the code the folder that you want to work with,
-after that it looks for all files in a higher format than 1920x1080 and converts them in 1920x1080 creating a new file that starts with
-NEW_{{Nameofthefile}} then deletes the old file and renames the new one to the old one
-NEXT STEP : include media conversion from HVEC to H264
+This program is pretty simple, it searches all file in the folder and subfolder given, converts all 4K content to 1080
+and all the HEVC to H624, then sleeps for X given seconds and tries again. That is used because if you add more
+content it can automatically detect it and convert it to the chosen resolution.
+## Execution
+The execution is pretty simple, in the code you set the number of thread that you wanna use, the delay
+and the folder that you wanna look inside (These are the first three constant) and you run it simply with
+python, important you must have ffmpeg on your system.
+I suggest to install this script on your linux machine as a service so it can run automatically
 ## License
 [AGPL-3.0](https://choosealicense.com/licenses/agpl-3.0/)
